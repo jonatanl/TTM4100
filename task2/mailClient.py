4dc8e41c4b9ae7b7a85399a5361a2ed7c3ae198a
Jonatan Lund <jonatan.lund.92@gmail.com>
2014-02-05 22:36:43 +0100

    Task 2 done!

--- task2/mailClient.py
+++ task2/mailClient.py
@@ -1,18 +1,18 @@
 from socket import * 
-msg = ”\r\n I love computer networks!” 
-endmsg = ”\r\n.\r\n” 
-# Choose a mail server (e.g. Google mail server) and call it mailserver 
-mailserver = #Fill in start #Fill in end 
-# Create socket called clientSocket and establish a TCP connection with mailserver 
-#Fill in start 
- 
-#Fill in end 
+
+msg = '\r\n I love computer networks!' 
+endmsg = '\r\n.\r\n'
+mailserver = 'smtp.stud.ntnu.no'
+port = 25
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver, port))
+
 recv = clientSocket.recv(1024) 
 print recv 
 if recv[:3] != '220': 
 	print '220 reply not received from server.' 
- 
-# Send HELO command and print server response. 
+
 heloCommand = 'HELO Alice\r\n' 
 clientSocket.send(heloCommand) 
 recv1 = clientSocket.recv(1024) 
@@ -20,19 +20,12 @@ print recv1
 if recv1[:3] != '250': 
 	print '250 reply not received from server.' 
 	
-# Send MAIL FROM command and print server response. 
-# Fill in start 
-
-# Fill in end 
- 
-# Send RCPT TO command and print server response. 
-# Fill in start 
- 
-# Fill in end 
- 
-# Send DATA command and print server response. 
-# Fill in start 
- 
-# Fill in end 
- 
-# Send message data. 
+clientSocket.send('MAIL FROM: jonatanl@stud.ntnu.no\r\n')
+clientSocket.send('RCPT TO: jonatanl@stud.ntnu.no\r\n') 
+clientSocket.send('DATA\r\n')
+clientSocket.send('Subject: test\r\n')
+clientSocket.send(msg + endmsg)
+print clientSocket.recv(1024) 
+clientSocket.send('QUIT\r\n')
+print clientSocket.recv(1024)
+clientSocket.close()
